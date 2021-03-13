20c4e5a718d0bdc26fd764ce4af3d52819ab8d92
BharatKarre <bharatKarre@gmail.com>
2021-03-13 21:21:09 +0530

    dashboard modified

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
+/*===== SHOW NAVBAR  =====*/ 
 const showNavbar = (toggleId, navId, bodyId, headerId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId),
     bodypd = document.getElementById(bodyId),
     headerpd = document.getElementById(headerId)
 
+    // Validate that all variables exist
     if(toggle && nav && bodypd && headerpd){
-        toggle.addEventListener('click', () => {
+        toggle.addEventListener('click', ()=>{
+            // show navbar
             nav.classList.toggle('show')
-
-            toggle.classList.toggle('fa-times')
-
+            // change icon
+            toggle.classList.toggle('bx-x')
+            // add padding to body
             bodypd.classList.toggle('body-pd')
-
+            // add padding to header
             headerpd.classList.toggle('body-pd')
         })
     }
 }
 
-showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+showNavbar('header-toggle','nav-bar','body-pd','header')
 
-// const linkColor = document.querySelectorAll('.nav__link')
+/*===== LINK ACTIVE  =====*/ 
+const linkColor = document.querySelectorAll('.nav__link')
 
-// function colorLink(){
-//     if(linkColor){
-//         linkColor.forEach(l=> l.classList.remove('active'))
-//         this.classList.add('active')
-//     }
-// }
-// linkColor.forEach(l=> l.addEventListener('click', colorLink))
-
-
-let btnback = document.querySelector('.back');
-
-btnback.addEventListener('click', () => {
-    window.history.back();
-});
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(l=> l.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
